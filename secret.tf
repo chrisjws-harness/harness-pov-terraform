5ac757dd981b24f37162be81e3c2df05929ade7a
Chris Storz <chris.storz@harness.io>
2023-01-12 16:49:18 -0800

    secret fix

--- secret.tf
+++ secret.tf
@@ -2,8 +2,8 @@ resource "harness_platform_secret_text" "textsecret" {
     identifier  = "example_secret"  
     name        = "example_secret"  
     description = "This is a text Secret, generated through Terraform"  
-    org_id      = "terraform_example_org"  
-    project_id  = "terraform_test_project"  
+    org_id      = var.org  
+    project_id  = var.project  
     tags        = ["example:tags"]  
   
     secret_manager_identifier = "harnessSecretManager"  
